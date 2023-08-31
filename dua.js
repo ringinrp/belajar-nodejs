1168881baa8eac7ebd578428f1a55188b6778cd0
ringinrp <ringinrp21@gmail.com>
2023-08-31 20:31:07 +0700

    first commit

---
+++ dua.js
@@ -0,0 +1,1 @@
+console.log(cetakNama('Ringin Restu Pati'));
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hallo");
+// const nama = "Ringin Restu Pati";
+// const cetakNama = (nama) => `Hai, nama saya ${nama}`
+// console.log(cetakNama(nama));
+
+const cetakNama = require('./coba');
+
+// console.log('Hallo Ringin');
+
+console.log(cetakNama('Ringin Restu Pati'));
