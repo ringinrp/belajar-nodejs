5ebfaf88a014692d0e13b36feef84852db73c216
ringinrp <ringinrp21@gmail.com>
2023-09-01 20:17:32 +0700

    update

--- coba.js
+++ coba.js
@@ -2,4 +2,25 @@ function cetakNama(nama){
     return `Halo, nama saya ${nama}`;
 }
 
-module.exports = cetakNama;
+const PI = 3.14;
+
+const mahasiswa = {
+    nama : 'Doddy',
+    umur : 23,
+    cetakMhs (){
+        return `Hallo, nama saya ${this.nama} dan saya umur saya ${this.umur} tahun`;
+    }
+};
+
+class Orang {
+    constructor(){
+        console.log('Object orang telah dibuat');
+    }
+};
+
+// module.exports.cetakNama = cetakNama;
+// module.exports.PI = PI;
+// module.exports.mahasiswa = mahasiswa;
+// module.exports.Orang = Orang;
+
+module.exports = { cetakNama, PI, mahasiswa, Orang };
--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 // const cetakNama = (nama) => `Hai, nama saya ${nama}`
 // console.log(cetakNama(nama));
 
-const cetakNama = require('./coba');
+// const fs = require('fs'); //core module
+// const cetakNama = require('./coba'); //local module
+// const PI = require('./coba');
+
+const coba = require('./coba');
+
 
 // console.log('Hallo Ringin');
 
-console.log(cetakNama('Ringin Restu Pati'));
+console.log(coba.cetakNama('Ringin Restu Pati'), coba.PI, coba.mahasiswa.cetakMhs(), new coba.Orang());
