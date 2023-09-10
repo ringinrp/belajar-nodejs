03f7b6c10eefd23d1350ad1eb181d778843847e6
ringinrp <ringinrp21@gmail.com>
2023-09-10 12:39:44 +0700

    update

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const fs = require('fs');
+const validator = require('validator');
+const chalk = require('chalk');
+// console.log(validator.isEmail('ringinrp@gmailm'));
+// console.log(validator.isMobilePhone('0812345678','id-ID'));
+const nama = 'Ringin Restu Pati';
+const pesan = chalk`dsaddassadsa {bgGreen sdaadadasd} asdsadsa sadsadas saddas {bgCyan asdasdsadsad} sadsadsad {bgYellow sadasd} dsasada. Nama : ${nama}`;
+console.log(pesan);
+
 
 //menuliskan string ke file (synchronous)
 // try{
@@ -7,7 +15,7 @@ const fs = require('fs');
 //     console.log(e);
 // }
 
-//menulisakn string ke filr asyn
+//menulisakn string ke file asyn
 // fs.writeFile('data/test.txt', 'Hello word secara asynchronous', (e)=>{
 //     console.log(e);
 // });
@@ -23,23 +31,23 @@ const fs = require('fs');
 // });
 
 //readline
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
 
-rl.question('Masukkan nama anda : ', (nama) => {
-    rl.question('Masukkan nomer HP anda : ', (noHP)=>{
-        const contact = {nama,noHP};
-        const file = fs.readFileSync('data/contacts.json', 'utf-8');
-        const contacts = JSON.parse(file); 
+// rl.question('Masukkan nama anda : ', (nama) => {
+//     rl.question('Masukkan nomer HP anda : ', (noHP)=>{
+//         const contact = {nama,noHP};
+//         const file = fs.readFileSync('data/contacts.json', 'utf-8');
+//         const contacts = JSON.parse(file); 
 
-        contacts.push(contact);
+//         contacts.push(contact);
 
-        fs.writeFileSync('data/contacts.json', JSON.stringify(contacts, null, 2));
+//         fs.writeFileSync('data/contacts.json', JSON.stringify(contacts, null, 2));
 
-        console.log(`Terima kasih ${nama} telah memasukkan nomer HP ${noHP}`);
-        rl.close();
-    });
-});
+//         console.log(`Terima kasih ${nama} telah memasukkan nomer HP ${noHP}`);
+//         rl.close();
+//     });
+// });
